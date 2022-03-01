f21cc9152b7c529c0cf38f26c160121af4826eb8
piaasbjornsen <pia.asbjornsen@icloud.com>
2022-03-01 15:58:30 +0100

    feat/#14: Create ui for group home page
    Co-Authored-By: Jonatan Norbye <jonatan@stud.ntnu.no>

---
+++ src/pages/mygroups/ThisGroup.tsx
@@ -0,0 +1,195 @@
+import {Grid, Button, CssBaseline, Typography} from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import React, {useEffect, useState} from 'react';
+import {groups as firebaseGroups} from '../../service/firebase';
+import {interests as firebaseInterests} from '../../service/firebase';
+import {users as firebaseUsers} from '../../service/firebase';
+import GroupsIcon from '@mui/icons-material/Groups';
+import {
+  IFirebaseDb,
+  IFirebaseGroup,
+  IFirebaseInterest,
+  IFirebaseUserId,
+  IFirebaseUserName,
+} from '../../interfaces/firebase';
+import {useParams} from 'react-router-dom';
+
+//Bruker i liste
+interface IUserListItem {
+  id: IFirebaseUserId;
+  name: IFirebaseUserName;
+}
+
+//Gruppeobjekt
+const emptyGroupObject = {
+  name: '',
+  description: '',
+  interests: [],
+  members: [],
+};
+
+const AddToList: React.FC = () => {
+  const [input, setInput] = useState<IFirebaseGroup>(emptyGroupObject);
+  const [interests, setInterests] = useState<IFirebaseInterest[]>([]);
+  const [users, setUsers] = useState<IUserListItem[]>([]);
+  const [group, setGroup] = useState<IFirebaseGroup | null>(null);
+  const [resetForm, setResetForm] = useState(false);
+
+  const urlParams = useParams();
+
+  useEffect(() => {
+    console.log(urlParams);
+
+    firebaseGroups.once('value', snapshot => {
+      const groups: IFirebaseDb['groups'] = snapshot.val();
+      if (groups[urlParams.groupId ?? ''] ?? false) {
+        setGroup(groups[urlParams.groupId ?? '']);
+        setInput(groups[urlParams.groupId ?? '']);
+        console.log(groups[urlParams.groupId ?? '']);
+
+        firebaseUsers.once('value', snapshot => {
+          const users: IFirebaseDb['users'] = snapshot.val();
+          const userList: IUserListItem[] = Object.keys(
+            users
+          ).map<IUserListItem>(userId => ({
+            id: userId,
+            name: users[userId].name,
+          }));
+          setUsers(userList);
+          setResetForm(!resetForm);
+        });
+      } else {
+        console.log('false');
+      }
+    });
+    firebaseInterests.once('value', snapshot => {
+      const interests = snapshot.val();
+      setInterests(interests);
+    });
+  }, []);
+
+  if (group === null || users === null || input === null) {
+    return <p>Laster inn</p>;
+  }
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ): void => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = (): void => {
+    if (urlParams?.groupId === '') {
+      console.log('Invalid group');
+      return;
+    }
+
+    const updatedData = {
+      // Mulig vi må endre denne, da det er en bug når vi oppdaterer siden.
+      ...group,
+      ...input,
+    };
+    console.log(updatedData);
+    firebaseGroups.child(urlParams?.groupId ?? '').update(updatedData);
+
+    setInput(emptyGroupObject);
+  };
+
+  return (
+    <>
+      <>
+        <CssBaseline />
+        <Grid container justifyContent="center" marginTop={5}>
+          {' '}
+          {/* Overskriften på siden, hentet fra react */}
+          <GroupsIcon fontSize="large" />
+          <Typography variant="h4" marginLeft={2}>
+            {group?.name}
+          </Typography>
+        </Grid>
+      </>
+      <Grid container justifyContent="center" marginTop={5}>
+        {' '}
+        {/* Beskrivelsen */}
+        <TextField
+          style={{width: 500}}
+          id="outlined-multiline-static"
+          variant="standard"
+          label="Beskrivelse"
+          multiline
+          rows={4}
+          inputProps={{maxLength: 240}}
+          onChange={handleChange}
+          defaultValue={group?.description}
+          name="description"
+          InputLabelProps={{shrink: true}}
+        />
+      </Grid>
+      <Grid container justifyContent="center" marginTop={5}>
+        <Autocomplete
+          key={'users' + resetForm}
+          id="addUsers"
+          multiple
+          freeSolo
+          style={{width: 500}}
+          onChange={(
+            event: React.SyntheticEvent,
+            value: (string | IUserListItem)[]
+          ) => {
+            const userIds = value.map(userListItem =>
+              typeof userListItem !== 'string' ? userListItem.id : userListItem
+            );
+            setInput({
+              ...input,
+              members: userIds,
+            });
+          }}
+          defaultValue={group?.members.map(userId => {
+            return (
+              users.find(userItem => userItem.id === userId) ?? {
+                id: '',
+                name: 'Ugyldig bruker',
+              }
+            );
+          })}
+          options={users}
+          getOptionLabel={option => option.name}
+          renderInput={params => (
+            <TextField {...params} label="Legg til medlemmer" />
+          )}
+        />
+      </Grid>
+      <Grid container justifyContent="center" marginTop={5}>
+        <Autocomplete
+          key={'interests' + resetForm}
+          id="addInterests"
+          multiple={true}
+          freeSolo
+          style={{width: 500}}
+          onChange={(event: React.SyntheticEvent, value: string[]) => {
+            setInput({
+              ...input,
+              interests: value,
+            });
+          }}
+          defaultValue={group?.interests}
+          options={interests}
+          renderInput={params => (
+            <TextField {...params} label="Legg til interesser" />
+          )}
+        />
+      </Grid>
+      <Grid container justifyContent="center" marginTop={5} marginBottom={10}>
+        <Button variant="contained" onClick={handleClick}>
+          Oppdater
+        </Button>
+      </Grid>
+    </>
+  );
+};
+
+export default AddToList;
